f6791b5e43ddb481b903dc4cb416c096a1257848
konstantinakanta <35781094+konstantinakanta@users.noreply.github.com>
2020-04-17 10:29:02 +0100

    Delete populatetables.sh

--- populatetables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed 1d Teams.csv | while IFS=, read -r team ranking games wins draws losses goalsFor goalsAgainst yellowCards redCards
-do
-   echo "INSERT INTO teams (team,ranking,games,wins,draws,losses,goalsFor,goalsAgainst,yellowCards,redCards) VALUES ('$team',$ranking,$games,$wins,$draws,$losses,$goalsFor,$goalsAgainst,$yellowCards,$redCards);"
-
-done | mysql -u mason --password=um8nv8EB7f -D mason;
