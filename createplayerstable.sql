42ab15f604de97d70823c20b5d592d3c1cc74b9a
konstantinakanta <35781094+konstantinakanta@users.noreply.github.com>
2020-04-17 10:28:12 +0100

    Delete createplayerstable.sql

--- createplayerstable.sql
+++
@@ -1,9 +0,0 @@
- create table players (surname varchar(60) not null,
- team varchar(60) not null,
- position varchar(60) not null,
- minutes smallint unsigned,
- shots smallint unsigned,
- passes smallint unsigned,
- tackles smallint unsigned,
- saves smallint unsigned,
- FOREIGN KEY (team) REFERENCES teams(team));
